ea29bf06ec6171b88cd8e163a8050665b32a37f4
pranjalshrivastav1012 <44617729+pranjalshrivastav1012@users.noreply.github.com>
2020-07-25 10:35:10 +0530

    half functioning ready

--- src/App.js
+++ src/App.js
@@ -1,21 +1,58 @@
-import React from 'react';
-import {Grid , Typography, Paper, Divider} from "@material-ui/core";
+import React, { Component } from 'react';
 
-class App extends React.Component {
-    render(){
-        return (
-           <Grid container style={{height: '100vh'}} justify="center" alignItems="center">
-               <Grid item xs={3}>
-                   <Paper style={{padding: '50px'}} elevation={6}>
-                       <Typography align="center" variant="h2" gutterBottom>HOT or COLD</Typography>
-                       <Divider style={{margin: '20px 0'}} />
-                   </Paper>
-               </Grid>
-           </Grid>
-        );
+import { Grid, Typography, Paper, Divider, Button } from '@material-ui/core';
+import { Form, Info, Progress } from './components';
+import { getInitialState, getFeedback } from './util';
+
+import './App.css';
+
+class App extends Component {
+    state = getInitialState();
+
+    resetGame = () => this.setState(getInitialState());
+
+    updateAppState = (guess) => {
+        const { actual } = this.state;
+
+        const absDiff = Math.abs(guess - actual);
+        const { feedbackMessage, feedbackColor } = getFeedback(absDiff);
+
+        this.setState(prevState => ({
+            guess,
+            allGuesses: [...prevState.allGuesses, { guess, feedbackColor }],
+            attempt: prevState.attempt + 1,
+            feedbackMessage,
+            block: absDiff === 0,
+        }));
     }
 
+    render() {
+        const { allGuesses, feedbackMessage, block, attempt, show } = this.state;
 
+        const guessList = allGuesses.map((item, index) => (
+            <li key={index}>
+                <span>{item.guess}</span>
+            </li>
+        ));
+
+        return (
+            <Grid style={{ height: '100vh' }} justify="center" alignItems="center" container>
+                <Grid item xs={3}>
+                    <Paper style={{ padding: '50px' }} elevation={6}>
+                        <Typography className="rainbow-text" align="center" variant="h2" gutterBottom>Guess My Number</Typography>
+                        <Divider style={{ margin: '20px 0' }} />
+                        <div className={`feedback ${feedbackMessage[0].toLowerCase()}`}>
+                            <h2 className="feedback-text">{feedbackMessage}</h2>
+                        </div>
+                        <Form block={block} returnGuessToApp={value => this.updateAppState(value)} />
+                        <Progress feedbackMessage={feedbackMessage} attempt={attempt} guessList={guessList} />
+
+                        <Info show={show} onClose={this.handleClose} />
+                    </Paper>
+                </Grid>
+            </Grid>
+        );
+    }
 }
 
 export default App;
---
+++ src/util/index.js
@@ -0,0 +1,35 @@
+export const generateRandomNumber = () => Math.floor(Math.random() * 100) + 1;
+
+export const getInitialState = () => ({
+    actual: generateRandomNumber(),
+    guess: undefined,
+    allGuesses: [],
+    attempt: 0,
+    feedbackMessage: 'Dare to guess...',
+    feedbackColor: '#434343',
+    block: false,
+});
+
+export const getFeedback = (absDiff) => {
+    let feedbackMessage;
+    let feedbackColor;
+
+    if (absDiff === 0) {
+        feedbackColor = '#7BFC1E';
+        feedbackMessage = 'Correct!';
+    } else if (absDiff <= 4 && absDiff >= 1) {
+        feedbackColor = '#F50727';
+        feedbackMessage = 'Hot!!';
+    } else if (absDiff >= 5 && absDiff <= 15) {
+        feedbackColor = '#F5E527';
+        feedbackMessage = 'Warm';
+    } else {
+        feedbackColor = '#5bc0de';
+        feedbackMessage = 'Cold';
+    }
+
+    return {
+        feedbackMessage,
+        feedbackColor,
+    };
+};
