db51e43c8981057395bf4f42d4aef8346e67086a
pranjalshrivastav1012 <44617729+pranjalshrivastav1012@users.noreply.github.com>
2020-07-26 03:26:44 +0530

    First Page text alignment done

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -3,8 +3,8 @@ import { NavLink } from 'react-router-dom'
 
 const Start = () => (
     <header className="start">
-        <h2 className="header">Welcome to the Game !</h2>
-        <NavLink className= "header__title" to="/play" activeClassName="is-active" exact={true}>Click here to Play</NavLink>
+        <h2 className="header">Welcome to Guessing Game !</h2>
+        <NavLink className= "header__title" to="/play" activeClassName="is-active" exact={true}>Start</NavLink>
 
     </header>
 );
